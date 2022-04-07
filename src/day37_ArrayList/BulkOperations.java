e62e1dedc55718f3e8ab8ff5eab65e075f1e1e64
Your Name <you@example.com>
2022-04-06 20:10:29 -0400

    Berkan

--- src/day37_ArrayList/BulkOperations.java
+++ src/day37_ArrayList/BulkOperations.java
@@ -1,5 +1,7 @@
 package day37_ArrayList;
 
+import day35_ArrayList.Combine2Arrays_List;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -25,10 +27,35 @@ public class BulkOperations {
         list2.add(3);
         list2.add(4);
         // Verify that 1,2, 4 are contained in the list2.
-        boolean r3 = list2.containsAll( Arrays.asList(1,2,4) );
-        Integer[] arr = {10,20,4};
+        boolean r3 = list2.containsAll(Arrays.asList(1, 2, 4));
+        Integer[] arr = {10, 20, 4};
         boolean r4 = list2.containsAll(Arrays.asList(arr));
+
+        System.out.println("==================");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        // add: 30, 25, 40, 15, 55, 65, 75, 85, 95, 100
+        //numbers.addAll(Arrays.asList(30, 25, 40, 15, 55, 65, 75, 85, 95, 100));
+        Integer[] nums = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 30, 25, 40, 15, 55, 65, 75, 85, 95, 100};
+        numbers.addAll(Arrays.asList(nums));
+        //System.out.println(numbers); it will give you this result = [30, 25, 40, 15, 55, 65, 75, 85, 95, 100]
+        System.out.println("======================");
+        ArrayList<Integer> num1 = new ArrayList<>();
+        num1.addAll(Arrays.asList(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9));
+        System.out.println(num1);
+        // only keep the elements that are either 1, 2, 3, or 4.
+        num1.retainAll(Arrays.asList(1, 2, 3, 9));
+        System.out.println(num1);
+        System.out.println("==============================================");
+        ArrayList<String> employees = new ArrayList<>();
+        employees.addAll(Arrays.asList("Ahmed", "Muhtar", "Ahmed", "Virginia", "Beslan", "Ibrahim", "Ahmed"));
+        System.out.println(employees);
+        employees.retainAll(Arrays.asList("Ahmet")); // only keep the matching element.
+        employees.removeAll(Arrays.asList("Ahmed")); // it will delete the matching element.
+        System.out.println(employees);
+
+
 
 
     }
-}
+    }
+
--- src/day37_ArrayList/BulkOperations_Practice.java
+++ src/day37_ArrayList/BulkOperations_Practice.java
@@ -1,7 +1,28 @@
 package day37_ArrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BulkOperations_Practice {
     public static void main(String[] args) {
+
+        ArrayList<String> students = new ArrayList<>();
+        students.add("Sayeem");
+        students.add("Waqar");
+        students.add("Beslan");
+        students.add("Dawud");
+        students.add("Ramazan");
+        students.add("Mehary");
+        // verify that the names Ulku, Busra are contained in students list.
+        boolean r1 = students.containsAll( Arrays.asList("Busra", "Ulku") );
+        System.out.println(r1);
+
+        System.out.println("=======================");
+        ArrayList<String> group1 = new ArrayList<>();
+        String[] names = {"Berkan", "Berkan1", "Berkan2", "Berkan3"};
+        group1.addAll(Arrays.asList(names));
+        boolean t1 = group1.containsAll(Arrays.asList("Muhtar", "Muhtar1"));
+        System.out.println(t1);
 
     }
 }
